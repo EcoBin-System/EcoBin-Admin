c47fe73ba8eb1dc127a4270962680f6144399561
Sahan Kaviska <it22233530@my.sliit.lk>
2024-10-14 01:44:23 +0530

    first commit

---
+++ ecobin_admin/lib/user_management/screens/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:ecobin_admin/user_management/models/UserModel.dart';
+import 'package:ecobin_admin/user_management/screens/authentication/authenticate.dart';
+import 'package:ecobin_admin/user_management/screens/home/home.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserModel?>(context);
+
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+  }
+}
